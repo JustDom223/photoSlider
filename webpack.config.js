65b01c09b8ddc36bb595db887cf0300eb47cf910
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-02-21 10:09:47 +0100

    cchecking that commits are working correctly

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "WebpackBoiler",
+            title: "Photo slider",
             filename: "index.html",
             template: "./src/template.html",
         }),
